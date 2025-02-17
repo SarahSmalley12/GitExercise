159128ef0c6edbb48467d9556bd178e00998e3cc
SarahSmalley12 <SmalleySA22@gcc.edu>
2025-02-17 15:24:54 -0500

    Git testing

--- src/TestGit.java
+++ src/TestGit.java
@@ -2,6 +2,7 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("How are you?");
+        System.out.println("Goodbye");
 
     }
 
