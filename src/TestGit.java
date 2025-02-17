ef4bd871d38ed4394003b1b08c81e78e7e1a9f60
SarahSmalley12 <SmalleySA22@gcc.edu>
2025-02-17 15:42:40 -0500

    Added to Git

--- src/TestGit.java
+++ src/TestGit.java
@@ -1,8 +1,8 @@
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hi");
-        System.out.println("How are you?");
-        System.out.println("Goodbyeee");
+        System.out.println("How are you");
+        System.out.println("today?");
     }
 
 }
